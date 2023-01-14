137a20482a05b0913d4dce54c031b8aa1567fe5b
jajablinky <gmillar6@gmail.com>
2023-01-14 13:46:50 -0800

    mapping out initial 3 blogs for testing and starting to move on fetching transactionID from arweave graphQL endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import useFetchBlogs from "./hooks/useFetchBlogs";
+import FetchTransactionId from "./hooks/FetchTransactionId";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // handle state of query, and pageNumber for graphQL request
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+
+  // Grab transactionID from aarweave Original-Content-Digest OR we can go by contributor using their eth address
+  const FetchTransactionId = () => {};
+  // Using that transactionID you're able to pull that post's content from graphQL endpoint
+  const { blogs, setBlogs } = useFetchBlogs();
+
+  // Infinite loading everytime you press the button it loads 3 more blogs
+  const handleLoadMore = (query, pageNumber) => {
+    set;
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {blogs.map((blog) => {
+        return (
+          <>
+            <h1 key={blog.title}>{blog.title}</h1>
+            <p key={blog.content}>{blog.content}</p>
+          </>
+        );
+      })}
+      <button onClick={handleLoadMore}>Load More</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/hooks/FetchTransactionId.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FetchTransactionId = () => {
+  return <div></div>;
+};
+
+export default FetchTransactionId;
